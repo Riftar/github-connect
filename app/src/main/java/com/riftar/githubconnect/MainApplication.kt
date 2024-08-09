4abc3aa4ef925222756330c624f74cd783ced956
rifqimukhtar <mukhtarrifqi@gmail.com>
2024-08-09 23:00:01 +0700

    feat: setup coil image loader, set cache, add logger

--- app/src/main/java/com/riftar/githubconnect/MainApplication.kt
+++ app/src/main/java/com/riftar/githubconnect/MainApplication.kt
@@ -1,11 +1,38 @@
 package com.riftar.githubconnect
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import com.riftar.githubconnect.di.KoinInitializer
 
-class MainApplication : Application() {
+class MainApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         KoinInitializer.init(this)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.20)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizeBytes(5 * 1024 * 1024)
+                    .build()
+            }
+            // to check where the image is coming from (DISK/MEMORY/NETWORK) on the logcat
+            .logger(DebugLogger())
+            .respectCacheHeaders(false)
+            .build()
     }
 }
